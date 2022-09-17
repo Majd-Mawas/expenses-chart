551af2395a72f2f5f2f4a17b5977886e148b261d
Majd Mawas <majosmws@gmail.com>
2022-09-17 21:34:32 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import datas from "/data.json" assert {type:'json'};
+const spendPercent = document.getElementById('spend-percent')
+// console.log(spendPercent);
+
+function render() {
+  let highPrice = 0;
+  for (let index = 0; index < datas.length; index++) 
+      highPrice=highPrice>datas[index].amount?highPrice:datas[index].amount
+
+    datas.forEach(element => {
+      spendPercent.innerHTML+=
+      `
+      <span style='
+      height:calc(${element.amount}px * 2.75);
+      background-color:${element.amount===highPrice?'hsl(186, 34%, 60%)':'hsl(10, 79%, 65%)'};'>
+      </span>
+      `
+  });
+}
+
+render()
+// console.log(datas);
